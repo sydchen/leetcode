05cce769a3515ca0d76b5eade0042ee5bc61bb69
Syd Chen <hello.sydd@gmail.com>
2016-01-19 18:58:16 +0800

    Repeated DNA Sequences

---
+++ repeated-dna-sequences.rb
@@ -0,0 +1,8 @@
+# https://leetcode.com/discuss/57528/1-liner-in-ruby
+# @param {String} s
+# @return {String[]}
+def find_repeated_dna_sequences(s)
+  s.chars.each_cons(10).group_by(&:join).select { |_, group| group.size > 1 }.keys
+end
+
+puts find_repeated_dna_sequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")
