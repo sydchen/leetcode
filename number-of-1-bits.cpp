0159b99d8cb5af26334df82383318e48d65720fb
Syd Chen <hello.sydd@gmail.com>
2016-02-17 01:03:06 +0800

    Number of 1 Bits

---
+++ number-of-1-bits.cpp
@@ -0,0 +1,23 @@
+/* https://en.wikipedia.org/wiki/Hamming_weight */
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int weight = 0;
+        for(; n ; n>>=1) {
+            if(n & 1)
+                weight += 1;
+        }
+        return weight;
+    }
+};
+
+int main(int argc, const char* argv[])
+{
+    Solution s;
+    cout << s.hammingWeight(11) << endl;
+}
