d8684819dfebe42f2dfe62926afbc638228c5c39
Syd Chen <hello.sydd@gmail.com>
2016-02-17 22:53:55 +0800

    House Robber

---
+++ house-robber.cpp
@@ -0,0 +1,23 @@
+/* https://leetcode.com/discuss/30020/java-o-n-solution-space-o-1 */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prevNo = 0, prevYes = 0, tmp;
+        for(auto n : nums) {
+            int tmp = prevNo;
+            prevNo = max(prevNo, prevYes);
+            prevYes = n + tmp;
+        }
+        return max(prevYes, prevNo);
+    }
+};
+
+int main(int argc, const char* argv[])
+{
+    Solution s;
+}
