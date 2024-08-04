f97be8b184c90172906360aa2f792b507d352fcd
Syd Chen <syd@mediaalpha.com>
2024-08-04 13:37:49 +0800

    Update description

--- longest-substring-with-at-least-k-repeating-characters.py
+++ longest-substring-with-at-least-k-repeating-characters.py
@@ -1,8 +1,9 @@
 #  Divide And Conquer
 # https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/solutions/87768/4-lines-python/
-# 很聰明漂亮的解法. 找出出現頻率小於k的字元, 如果找不到代表最大解
-# 如果找到, 根據該字元c, 切成多個sub strings, 因為substring不會包含該字元
-# 找出substrings中是否有符合條件的, ex: s=acccabbfb, k = 3, c=a => substrings=[ccc, bbfb], 找到 ccc符合
+# 很聰明漂亮的解法. 找出出現頻率小於k的字元, 如果都大於k, 找到最大解
+# 接著根據該字元c, 切成多個sub strings, 因為sub strings不會包含該字元
+# 找出sub strings中是否有符合條件的, ex: s=acccabbfb, k = 3, c = a => substrings=[ccc, bbfb] => 3
+# s = ababbc, k = 3, c = a => substrings=[b, bbc] => 0
 
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
