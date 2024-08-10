25fe39d21c3a6e384442dc2f3df5f90918d57da6
Syd Chen <syd@mediaalpha.com>
2024-08-11 02:21:59 +0800

    15. 3Sum - python3

---
+++ 3sum.py
@@ -0,0 +1,52 @@
+from typing import List
+from collections import defaultdict
+
+# Trivil Cases
+# Case 1: 3 zeros (0, 0, 0)
+# Case 2: 1 zero, 1 positive and 1 negative (0, 3, -3)
+# Case 3: 1 positive and 2 negative (6, -2, -4)
+# Case 4: 2 positive and 1 negative (2, 4, -6)
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        negatives = defaultdict(int)
+        positives = defaultdict(int)
+        zeros = 0
+        ans = []
+
+        for n in nums:
+            if n > 0:
+                positives[n] += 1
+            elif n == 0:
+                zeros += 1
+            else:
+                negatives[n] += 1
+
+        # Case 2
+        if zeros > 0:
+            for p in positives:
+                if -p in negatives: ans.append([-p, 0, p])
+
+            if zeros >= 3: ans.append([0, 0, 0])
+
+        # Case 3 and Case 4
+        for set_a, set_b in ((negatives, positives), (positives, negatives)):
+            lst = list(set_a.keys())
+            length = len(lst)
+            combinations = []
+            for i in range(0, length):
+                if set_a[lst[i]] > 1: combinations.append((lst[i], lst[i]))
+                for j in range(i + 1, length):
+                    combinations.append((lst[i], lst[j]))
+
+            for x, y in combinations:
+                target = x + y
+                if -target in set_b: ans.append(sorted([-target, x, y]))
+
+        return ans
+
+s = Solution()
+print(s.threeSum([-1,0,1,2,-1,-4]))
+print(s.threeSum([0,1,1]))
+print(s.threeSum([0,0,0]))
+print(s.threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
